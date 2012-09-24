3f8013fa9add9df94366af569179000d796ed7a9
EYE <eye.zhi@gmail.com>
2012-09-25 01:59:20 +0800

    精简调试信息。ping延迟<1ms。

--- src/lib/lwip_lpc/config/lwipopts.h
+++ src/lib/lwip_lpc/config/lwipopts.h
@@ -110,9 +110,9 @@
 #define LWIP_DEBUG
 
 #define TCP_DEBUG                       LWIP_DBG_ON
-#define ETHARP_DEBUG                    LWIP_DBG_ON
-#define PBUF_DEBUG                      LWIP_DBG_ON
-#define IP_DEBUG                        LWIP_DBG_ON
+#define ETHARP_DEBUG                    LWIP_DBG_OFF
+#define PBUF_DEBUG                      LWIP_DBG_OFF
+#define IP_DEBUG                        LWIP_DBG_OFF
 #define TCPIP_DEBUG                     LWIP_DBG_OFF
 #define DHCP_DEBUG                      LWIP_DBG_OFF
 #define UDP_DEBUG                       LWIP_DBG_ON
